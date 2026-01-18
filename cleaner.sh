8762080bc117bff2fd14820ab9012b66b6d0d63e
Xenia <xenos@odinsova.localdomain>
2026-01-18 23:43:42 +0300

    Upgrade: Add autoremover

---
+++ cleaner.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+echo "======================================"
+echo " Cleaner activated "
+echo "======================================"
+
+rm -r dist/
+rm main_word
+rm -r package/
+rm -r  usr/
+
+
+echo "======================================"
+echo "Removed!"
+echo "======================================"
